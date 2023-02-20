364037322c00ed544b36d40437c02ec192a4fedb
Tyler <tscummi@ilstu.edu>
2023-02-20 17:17:29 -0600

    Added comment

--- src/App.js
+++ src/App.js
@@ -1,76 +1,76 @@
-import React from 'react';
-import { useState } from 'react';
-import BlogForm from './components/BlogForm';
-import RemovePost from './components/RemovePost';
-import posts from './components/Posts';
-import 'bootstrap/dist/css/bootstrap.css';
-import './style.css';
-const axios = require('axios').default;
+import React from "react";
+import { useState } from "react";
+import BlogForm from "./components/BlogForm";
+import RemovePost from "./components/RemovePost";
+import posts from "./components/Posts";
+import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
+const axios = require("axios").default;
 
 export default function App() {
-  const [users, setUsers] = useState(posts);
+    const [users, setUsers] = useState(posts);
 
-  function getPosts() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(function (response) {
-        console.log('GET Success!');
-        console.log(response);
-        const shortdArray = response.data.slice(0, 3);
-        setUsers(shortdArray);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
+    function getPosts() {
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then(function (response) {
+                console.log("GET Success!");
+                console.log(response);
+                const shortdArray = response.data.slice(0, 3);
+                setUsers(shortdArray);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+    //submits posts
+    const submitPost = (user) => {
+        const num = Math.floor(Math.random() * 10000) + 1;
+        const newUser = { num, ...user };
 
-  const submitPost = (user) => {
-    const num = Math.floor(Math.random() * 10000) + 1;
-    const newUser = { num, ...user };
+        console.log(user);
+        console.log(user["user"]);
+        console.log(user["title"]);
+        console.log(user["post"]);
 
-    console.log(user);
-    console.log(user['user']);
-    console.log(user['title']);
-    console.log(user['post']);
+        axios
+            .post("https://jsonplaceholder.typicode.com/posts", {
+                user: user["user"],
+                title: user["title"],
+                post: user["post"],
+            })
+            .then((response) => {
+                console.log("POST Success!");
+                console.log(response);
+                setUsers([...users, newUser]);
+            });
+    };
 
-    axios
-      .post('https://jsonplaceholder.typicode.com/posts', {
-        user: user['user'],
-        title: user['title'],
-        post: user['post'],
-      })
-      .then((response) => {
-        console.log('POST Success!');
-        console.log(response);
-        setUsers([...users, newUser]);
-      });
-  };
+    const deletePost = (num) => {
+        axios
+            .delete(`${"https://jsonplaceholder.typicode.com/posts"}/1`)
+            .then(() => {
+                console.log("DELETE Success!");
+                setUsers(users.filter((user) => user.id !== num));
+            });
+    };
 
-  const deletePost = (num) => {
-    axios
-      .delete(`${'https://jsonplaceholder.typicode.com/posts'}/1`)
-      .then(() => {
-        console.log('DELETE Success!');
-        setUsers(users.filter((user) => user.id !== num));
-      });
-  };
+    return (
+        <div class="container">
+            <div class="row">
+                <BlogForm onAdd={submitPost} />
+                <div class="col-md-6">
+                    <button class="btn btn-success mb-4" onClick={getPosts}>
+                        GET Request
+                    </button>
+                </div>
 
-  return (
-    <div class="container">
-      <div class="row">
-        <BlogForm onAdd={submitPost} />
-        <div class="col-md-6">
-          <button class="btn btn-success mb-4" onClick={getPosts}>
-            GET Request
-          </button>
+                {users.length > 0 ? (
+                    <RemovePost users={users} onDelete={deletePost} />
+                ) : (
+                    "There are currently no posts"
+                )}
+            </div>
         </div>
-
-        {users.length > 0 ? (
-          <RemovePost users={users} onDelete={deletePost} />
-        ) : (
-          'There are currently no posts'
-        )}
-      </div>
-    </div>
-  );
+    );
 }
